4b3c6a678bedb8f37b8268452a6ed8bf1c51fa63
cintya <rmishel564@gmail.com>
2023-04-20 15:24:44 -0500

    se dan estilos a las cards y titulo

--- src/App.js
+++ src/App.js
@@ -5,25 +5,25 @@ function App() {
     <p className="title1">Our Pricing</p>
     <div className="container-cards">
       <Card
-        title="basic"
+        title="Basic"
         price=" 19.99"
         storage="500 GB Storage"
         allowed=" 2 Users Allowed"
         send="Send up to 3 GB"
       />
       <Card
-        title="basic"
-        price="$ 19.99"
-        storage="500 GB Storage"
-        allowed=" 2 Users Allowed"
-        send="Send up to 3 GB"
+        title="Professionl"
+        price="24.99"
+        storage="1 TB Storage"
+        allowed=" 5 Users Allowed"
+        send="Send up to 10 GB"
       />
       <Card
-        title="basic"
-        price="$ 19.99"
-        storage="500 GB Storage"
-        allowed=" 2 Users Allowed"
-        send="Send up to 3 GB"
+        title="Master"
+        price=" 39.99"
+        storage="2 TB Storage"
+        allowed=" 10 Users Allowed"
+        send="Send up to 20 GB"
       />
     </div>
   </div>;
--- src/component/Cards.jsx
+++ src/component/Cards.jsx
@@ -5,11 +5,11 @@ function Card(props) {
     return(
         <div className="card">
            <p className="title"> {props.title} </p>
-           <p className="price"><p className="simbolo"></p>${props.price}</p>
+           <p className="price">${props.price}</p>
            <p className="storage">{props.storage}</p>
            <p className="allowed">{props.allowed}</p>
            <p className="send">{props.send}</p>
-           <button>LEARN MORE</button>
+           <button className="btn">LEARN MORE</button>
         </div>
     );
 }
