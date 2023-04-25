989126e03dbc0f2edbf4646067702994af65f9b4
cintya <rmishel564@gmail.com>
2023-04-24 20:57:21 -0500

    se da funcion cmpleta al checkbox con usestate

--- src/App.js
+++ src/App.js
@@ -1,47 +1,51 @@
 import Card from "../src/component/cards/Cards";
 import "bootstrap";
 import Check from "./component/checkbox/Check";
+import { useState } from "react";
 
+const cardsProperty = [
+  {
+    title: "Basic",
+    price: " 19.99",
+    price2: " 30.99",
+    storage: "500 GB Storage",
+    allowed: " 2 Users Allowed",
+    send: "Send up to 3 GB",
+  },
+  {
+    title: "Professionl",
+    price: "24.99",
+    price2: " 30.99",
+    storage: "1 TB Storage",
+    allowed: " 5 Users Allowed",
+    send: "Send up to 10 GB",
+  },
+  {
+    title: "Master",
+    price: " 39.99",
+    price2: " 30.99",
+    storage: "2 TB Storage",
+    allowed: " 10 Users Allowed",
+    send: "Send up to 20 GB",
+  }
+]
 
 function App() {
 
-  const cardsProperty = [
-    {
-      title: "Basic",
-      price: " 19.99",
-      price2: " 30.99",
-      storage: "500 GB Storage",
-      allowed: " 2 Users Allowed",
-      send: "Send up to 3 GB",
-    },
-    {
-      title: "Professionl",
-      price: "24.99",
-      price2: " 30.99",
-      storage: "1 TB Storage",
-      allowed: " 5 Users Allowed",
-      send: "Send up to 10 GB",
-    },
-    {
-      title: "Master",
-      price: " 39.99",
-      price2: " 30.99",
-      storage: "2 TB Storage",
-      allowed: " 10 Users Allowed",
-      send: "Send up to 20 GB",
-    }
-  ]
+  const [checkbox, setCheckbox] = useState(true)
 
   return <div className="container">
     <p className="title1">Our Pricing</p>
 
-    <Check />
+    <Check handlChecked={setCheckbox} />
 
     <div className="container-cards">
       {cardsProperty.map((property) => {
+
+        const { price, price2 } = property
         return (
           <Card title={property.title}
-            price={property.price}
+            price={checkbox ? price : price2}
             storage={property.storage}
             allowed={property.allowed}
             send={property.send} />
--- src/component/checkbox/Check.jsx
+++ src/component/checkbox/Check.jsx
@@ -1,21 +1,26 @@
-//import react, {useState} from "react";
-import style from "../../component/checkbox/stylecheck.css";
+import react from "react"
+import style from "../../component/checkbox/stylecheck.css"
+import { useState } from "react";
 
 
 
+function Check(props) { 
+    const {handlChecked} = props 
 
-function Check() {
+
     const handleCheckbox = () => {
         const valueCheckbox = window.document.querySelector("#switch").checked
         console.log(valueCheckbox)
+       handlChecked (valueCheckbox)
 
     }
+
 
     return (
         <div className="container-check">
             <p className="anual"> Annually   </p>
             <label class="switch">
-                <input onClick={handleCheckbox}  id="switch"  type="checkbox"/>
+                <input onClick={handleCheckbox} id="switch"  type="checkbox"/>
                     <span class="slider round"></span>
             </label>
             <p className="mes">Monthly</p>
